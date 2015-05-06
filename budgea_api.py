4620a08298a0773d265fa8a637881d407f442cf1
Romain Bignon <romain@budget-insight.com>
2015-05-06 11:49:08 +0200

    fix typo

--- budgea_api.py
+++ budgea_api.py
@@ -12,7 +12,7 @@
 # GNU General Public License for more details.
 #
 # You should have received a copy of the GNU Affero General Public License
-# along with this progrram. If not, see <http://www.gnu.org/licenses/>.
+# along with this program. If not, see <http://www.gnu.org/licenses/>.
 
 
 import simplejson
